4a0bd53e2e5c84e00e5ca5372bf8abbd0a50d05e
ferin <sudawan91@gmail.com>
2025-03-30 21:34:07 +0700

    create header

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 export default function ContactPage() {
   return (
-    <main>
+    <main className="mt-20">
       <p className="">Hello contact page</p>
+      <Button variant="outline" asChild>
+        <Link href="/">Go to home page</Link>
+      </Button>
     </main>
   );
 }
